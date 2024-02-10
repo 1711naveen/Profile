0c55d1299f57bd13ddc1e64adf6dae01f62cca10
Naveen Yadav <yn.naveen00@gmail.com>
2024-02-10 23:13:05 +0530

    Added Functionality to Download Button to download Resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -6,12 +6,8 @@ import resume from '../../Assets/Naveen_Yadav-MMMUT.pdf'
 import { pdfjs } from 'react-pdf';
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { Link } from 'react-router-dom';
 
-
-// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-//     'pdfjs-dist/build/pdf.worker.min.js',
-//     import.meta.url,
-// ).toString();
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -19,15 +15,19 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const Resume = () => {
 
     return (
-        <div className='set-font' style={{fontSize:"1.2em"}}>
+        <div className='set-font' style={{ fontSize: "1.2em" }}>
             <div className='container custom-resume text-center'>
-                <button type="button" className="btn btn-success mb-5" ><FaFileDownload className='me-3 mb-1' /><strong>Download Resume</strong></button>
+                <Link to={resume} target='_blank'>
+                    <button type="button" className="btn btn-success mb-5" ><FaFileDownload className='me-3 mb-1' /><strong>Download Resume</strong></button>
+                </Link>
                 <div className="row">
                     <Document file={resume} className="d-flex justify-content-center" >
                         <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} scale={1.5} />
                     </Document>
                 </div>
-                <button type="button" className="btn btn-success mt-5" ><FaFileDownload className='me-3 mb-1' /><strong>Download Resume</strong></button>
+                <Link to={resume} target='_blank'>
+                    <button type="button" className="btn btn-success mt-5 " ><FaFileDownload className='me-3 mb-1' /><strong>Download Resume</strong></button>
+                </Link>
             </div>
         </div>
     )
